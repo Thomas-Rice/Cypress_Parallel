25968423f4d8fb65d3a7dce0ae3e8fcdd87dbb7e
thomas-rice <tikkleanim@gmail.com>
2020-03-05 16:04:45 -0500

    First Commit

---
+++ Parallel_Runner.py
@@ -0,0 +1,51 @@
+import docker
+import os
+
+spec_directory = "C:\\Users\\tom.rice\\Documents\\Cypress\\Cypress_Project\\cypress\\integration"
+
+
+def get_spec_files(directory):
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(directory):
+
+        for json_file in f:
+            if '.spec.js' in json_file:
+                if r == spec_directory:
+                    files.append(json_file)
+                else:
+                    full_path = os.path.join(r, json_file)
+                    new_path = full_path.split(spec_directory)
+
+                    files.append(new_path[-1].replace('\\', '/'))
+
+    print("Discovered The Following Files To Use As Specs:")
+    for f in files:
+        print(f)
+    return files
+
+
+local_volume_to_map_to = 'C:\\Users\\tom.rice\\Documents\\Cypress\\Cypress_Project'
+desired_containers = 2
+counter = 0
+client = docker.from_env()
+specs = get_spec_files(spec_directory)
+
+while counter < len(specs):
+    if len(client.containers.list()) < desired_containers:
+        client.containers.run('cypress-tom',
+                              remove=True,
+                              environment={"testtorun": specs[counter]},
+                              detach=True,
+                              volumes={local_volume_to_map_to: {'bind': '/e2e', 'mode': 'rw'}}
+                              )
+        counter += 1
+
+for container in client.containers.list(all=True):
+    container.stop()
+    container.remove()
+
+
+# specs = ["bestbuy.spec.js", "sample.spec.js", "/examples/actions.spec.js", "/examples/aliasing.spec.js",
+#          "/examples/assertions.spec.js",
+#          "/examples/connectors.spec.js"]
